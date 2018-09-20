df973f9368a95b7f4e4f9300dfc302dcbc5f7c6e
xustinchenko <evgeniy.ustinchenko@gmail.com>
2018-09-20 21:08:54 +0300

    1

--- BakalarskaPraca/ServerAOC/src/main/java/Main.java
+++ BakalarskaPraca/ServerAOC/src/main/java/Main.java
@@ -1,15 +1,16 @@
 
 import database.Database;
 import nn.TestNeuralNetwork;
+import server.Server;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         Database database = new Database();
         database.connectToDatabase();
-        new TestNeuralNetwork().startTest();
-        /*server.Server server = server.Server.getServer();
+        //new TestNeuralNetwork().startTest();
+        Server server = Server.getServer();
         Thread t = new Thread(server);
-        t.start();*/
+        t.start();
     }
 }
